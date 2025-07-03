8b0653a852abdb880c148507171873128836dee7
binsoy69 <binslegendary@gmail.com>
2025-07-03 18:40:11 +0800

    Update to Neon persistent PostgreSQL

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from .models import db
 from .routes import bp
+import os
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'  # Change to MySQL later if needed
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        'DATABASE_URL', 'sqlite:///urls.db'
+    )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
