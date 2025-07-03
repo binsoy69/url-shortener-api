669d96885dcc0fe398db3c40fb14940376e51cf9
binsoy69 <binslegendary@gmail.com>
2025-07-03 19:23:33 +0800

    Update run.py to use environment variable for port configuration

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
